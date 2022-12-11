643fca354da62543b69242ec699d6a8c644d8730
Elisabeth Sáez <eesaez.munoz@gmail.com>
2022-12-11 15:29:26 -0300

    Manejo de archivos

--- ProductManager.js
+++ ProductManager.js
@@ -1,16 +1,23 @@
-/* Realizar una clase “ProductManager” que gestione un conjunto de productos. */
+const fs = require('fs')
 
 class ProductManager {
+    constructor(path) {
+        this.path = path;
+        try {
+            this.objs = fs.readFileSync(this.path, 'utf-8');
+            this.objs = JSON.parse(this.objs)
 
-    constructor() {
-        this.products = [];
+        } catch (error) {
+            this.objs = []
+        }
     }
 
     getProducts() {
-        return this.products;
+        return this.objs
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
+
+    async addProduct(title, description, price, thumbnail, code, stock) {
         let product = {
             title,
             description,
@@ -23,52 +30,96 @@ class ProductManager {
         //campos vacios
         for (const key in product) {
             if (Object.hasOwnProperty.call(product, key)) {
-                const element = product[key];
-                if (!element) {
-                    console.log(`${key}:${element}`);
+                const e = product[key];
+                if (!e) {
+                    console.log(`Al producto ${title} tiene el siguiente problema: ${key}:${e}`);
                     return;
                 }
             }
         }
-
         //existe code
-        for (let i = 0; i < this.products.length; i++) {
-            const element = this.products[i];
+        for (let i = 0; i < this.objs.length; i++) {
+            const element = this.objs[i];
             if (element.code == code) {
                 console.log(`El code ${code} ya existe, por favor, ingresa otro code para el producto ${title}`);
                 return;
             }
         }
-
         //valida id
-        if (this.products.length === 0) {
+        if (this.objs.length === 0) {
             product['id'] = 1
         } else {
-            product['id'] = this.products[this.products.length - 1]['id'] + 1
+            product['id'] = this.objs[this.objs.length - 1]['id'] + 1
         }
+        this.objs.push(product)
 
 
-        this.products.push(product);
 
-
+        try {
+            await fs.promises.writeFile(this.path, JSON.stringify(this.objs, null, '\t'))
+            console.log('Producto guardado')
+        } catch (error) {
+            console.log('Error al guardar:', error)
+        }
     }
 
     getProductById(id) {
-        let obj = this.products.find(o => o.id === id);
+        let obj = this.objs.find(o => o.id === id);
         if (obj) {
             console.log("----------------------------");
             console.log(obj);
         } else {
-            console.log("Product not found");
+            console.log("Producto no encontrado");
+        }
+
+    }
+
+    async deleteProduct(id) {
+        try {
+            const obj = this.objs.findIndex((elm) => elm.id === id)
+            if (obj !== -1) {
+                this.objs.splice(obj, 1)
+                await fs.promises.writeFile(this.path, JSON.stringify(this.objs, null, '\t'))
+                console.log('Producto eliminado')
+            } else {
+                console.log('No existe producto')
+            }
+        } catch (error) {
+            console.log('Error al eliminar id', error)
+        }
+    }
+
+    async update(id, element) {
+        try {
+            const oldElement = this.objs.find((element) => element.id === id)
+            const index = this.objs.findIndex((elemento) => elemento.id === id)
+
+            if (index !== -1) {
+                const newElement = { ...oldElement, ...element }
+                this.objs[index] = newElement
+                await fs.promises.writeFile(this.path, JSON.stringify(this.objs, null, '\t'))
+                console.log('Producto actualizado')
+            }
+        } catch (error) {
+            console.log('Error update:', error)
         }
     }
 
 }
 
-const producto = new ProductManager();
-producto.addProduct("manzana", "fruta", 56, "Sin imagen", 12, 40);
-producto.addProduct("Agua", "bebida", 30, "Sin imagen", 12, 25); //code repetido
-producto.addProduct("pera", "fruta", 48, "Sin imagen", 13, 60);
-producto.addProduct("Melón", "fruta",25, "Sin imagen", 20); //falta el stock
-console.log(producto.getProducts());
-producto.getProductById(1) // busca el id 1
+const producto = new ProductManager('./products.json')
+
+/* producto.addProduct("manzana", "fruta", 56, "Sin imagen", 88, 40);
+producto.addProduct("Libreta", "papeleria", 26, "Sin imagen", 88, 50); //code repetido
+producto.addProduct("Agua", "bebida", 30, "Sin imagen", 33, 25);
+producto.addProduct("pera", "fruta", 48, "Sin imagen", 59, 60);
+producto.addProduct("Melón", "fruta", 25, "Sin imagen", 20); //falta el stock
+producto.addProduct("Jabón", "Producto de limpieza", 150, "Sin imagen", 11, 30);
+ */
+//producto.getProductById(3) // trae objeto según id indicado a la consola
+
+//console.log(producto.getProducts());//trae productos existentes a la consola
+
+//producto.update(3, { price: 78 }); //modifica el precio
+
+//producto.deleteProduct(2) //Elimina productos según id
